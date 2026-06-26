75ffc404b82e4c183c07982abf2cc1419c7780a7
Pradeep03k <pradip87666646528@gmail.com>
2026-06-26 20:51:58 +0530

    some duplicate file deleted

--- BASIC PROGRAM/14th.py
+++
@@ -1,5 +0,0 @@
-Voter_Name=input("Enter Voter Name : ")
-Citizenship=input("Enter Citizenship : ")
-Criminal_record=input("Enter Criminal record : ")
-
-if Citizenship:
--- basic programs/1st.py
+++
@@ -1,4 +0,0 @@
-print("hello world")
-a=20
-print(a)
-print(type(a))
